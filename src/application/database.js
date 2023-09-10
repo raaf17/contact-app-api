5ea9dd9dea214ad0dbb099bd778c19ad57f7d44a
raaf17 <raafimuhammadd123@gmail.com>
2023-09-11 00:05:12 +0700

    set up folder application

---
+++ src/application/database.js
@@ -0,0 +1,38 @@
+import { prismaClient } from "@prisma/client";
+import { logger } from "./logging";
+export const prismaClient = new prismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'event',
+      level: 'error',
+    },
+    {
+      emit: 'event',
+      level: 'info',
+    },
+    {
+      emit: 'event',
+      level: 'warn',
+    },
+  ],
+});
+
+prismaClient.$on('error', (e) => {
+  logger.error(e);
+});
+
+prismaClient.$on('warn', (e) => {
+  logger.warn(e);
+});
+
+prismaClient.$on('info', (e) => {
+  logger.info(e);
+});
+
+prismaClient.$on('query', (e) => {
+  logger.info(e);
+});
---
+++ src/application/logging.js
@@ -0,0 +1,8 @@
+import winston from "winston";
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transport.Console({})
+  ]
+});
